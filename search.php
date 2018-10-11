d9e94f06ed5243c42030dfa9bcf8beb84d38bc00
Steve <cordovas@bc.edu>
2018-10-10 22:15:57 -0400

    more files

--- search.php
+++ search.php
@@ -1,5 +1,8 @@
 <?php
 
+require 'helpers.php';
+
+dump('whatever');
 # 1. Get the Data from the form request
 # 2.Load the books and them filter them based on the search term
 # 3.Store the results in the SESSION
@@ -7,31 +10,31 @@
 
 //require 'Book.php';
 
-session_start();
-
-require 'helpers.php';
-
-use Foobooks0\Book;
-
-//$book = new Book('books.json');
-
-die();
-
-# Get data from form request
-$searchTerm = $_POST('searchTerm');
-$caseSensitive = isset($_POST['caseSensitive']);
-
-$books = $book->getByTitle($caseSensitive, $searchTerm);
-
-# Store our results data in the SESSION
-
-$_SESSION['results'] = [
-    'searchTerm' => $searchTerm,
-    'books' => $books,
-    'bookCount' => count($books),
-    'caseSensitive' => $caseSensitive
-
-];
-
-header('location: index.php');
+//session_start();
+//
+//require 'helpers.php';
+//
+//use Foobooks0\Book;
+//
+////$book = new Book('books.json');
+//
+//die();
+//
+//# Get data from form request
+//$searchTerm = $_POST('searchTerm');
+//$caseSensitive = isset($_POST['caseSensitive']);
+//
+//$books = $book->getByTitle($caseSensitive, $searchTerm);
+//
+//# Store our results data in the SESSION
+//
+//$_SESSION['results'] = [
+//    'searchTerm' => $searchTerm,
+//    'books' => $books,
+//    'bookCount' => count($books),
+//    'caseSensitive' => $caseSensitive
+//
+//];
+//
+//header('location: index.php');
 
