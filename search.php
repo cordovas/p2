2ee2323b9d0bef5a5ba23d9c75252e0c398599a0
Steve <cordovas@bc.edu>
2018-10-11 13:30:29 -0400

    further cleanup

--- search.php
+++ search.php
@@ -1,11 +1,14 @@
 <?php
 
 require 'helpers.php';
+require 'Form.php';
 
 //dump($_GET);
 //die();
 
 session_start();
+
+$form = new DWA\Form($_GET);
 
 $totalNumbers = $_GET['totalNumbers'];
 $randomNumbers = $_GET['randomNumbers'];
